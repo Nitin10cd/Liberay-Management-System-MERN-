7a4a94abf422c84363e2c282661a45941fd5e912
Nitin Saxena <144582475+Nitin10cd@users.noreply.github.com>
2024-11-20 02:35:47 +0530

    Update app.js

--- .github/Back-end/app.js
+++ .github/Back-end/app.js
@@ -1,1 +1,24 @@
 
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('./Models/connection');
+const userRoutes = require('./Routes/Userrouter');
+require('dotenv').config();
+const bookArr = require('./Models/BookApi.js');
+const BookModel = require('./Models/Book.model.js');
+const AdminRouter = require('./Routes/Adminroter.js');
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+app.use('/api/users', userRoutes);
+app.use('/api/book', AdminRouter);
+
+app.listen(3002, () => {
+  console.log('Server running at http://localhost:3002');
+});
