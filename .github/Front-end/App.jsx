3d51d2ca6bb4e0ad56997fbe54b466308e4472c5
Nitin Saxena <144582475+Nitin10cd@users.noreply.github.com>
2024-11-20 02:19:12 +0530

    Update App.jsx

--- .github/Front-end/App.jsx
+++ .github/Front-end/App.jsx
@@ -1,1 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from '../Components/Login';
+import Register from '../Components/Register';
+import Userhome from '../Components/Userhome';
+import Adminhome from '../Components/Adminhome';
+import { useState, useEffect } from 'react';
 
+function App() {
+  const [userId, setUserId] = useState('');
+  
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setUserId(token);
+    }
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login userId={userId} setUserId={setUserId} />} />
+        <Route path="/" element={<Register />} />
+        <Route path="/user/home" element={<Userhome userId={userId} setUserId={setUserId} />} />
+        <Route path="/admin/home" element={<Adminhome />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
